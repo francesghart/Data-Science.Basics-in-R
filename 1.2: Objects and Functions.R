e277dadd120b7b755259659703ada0f548f89c96
hartfa <hartfa@bc.edu>
2023-06-04 22:53:08 -0400

    Objects and Functions

---
+++ 1.2: Objects and Functions.R
@@ -0,0 +1,37 @@
+a <- 1
+b <- 2
+c <- 3
+
+print(a)
+
+ls()
+
+log(8)
+log(a)
+
+exp(1)
+log(2.718)
+
+log(exp(1))
+
+help(log)
+?log 
+args(log)
+log(8,base=2)
+log(8,2)
+
+2^3
+
+data()
+CO2
+
+##Code to compute solution to quadratic
+##equation of the form ax^2 + bx + c
+##define the variables
+a <- 3
+b <- 2
+c <- -1
+
+##now compute the solution
+(-b + sqrt(b^2 - 4*a*c))/(2*a)
+(-b - sqrt(b^2 - 4*a*c))/(2*a)
