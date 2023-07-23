ff411a83c81c9a20112dc14af09178d6d35d6505
hartfa <hartfa@bc.edu>
2023-07-22 22:28:04 -0400

    Pull to access columns, the dot placeholder, grouped data frames, sorting data tables

--- 3.4 summarizing with dplyr.R
+++ 3.4 summarizing with dplyr.R
@@ -50,3 +50,91 @@ my_quantile <- function(x){
 murders %>% 
   filter(region == "West") %>%
   summarize(my_quantile(rate))
+
+## Pull to access columns
+library(tidyverse)
+library(dplyr)
+library(dslabs)
+data(murders)
+murders <- mutate(murders, rate = total / population * 10^5)
+
+# average rate adjusted by population size
+us_murder_rate <- murders %>% 
+  summarize(rate = sum(total) / sum(population) * 10^5)
+us_murder_rate
+
+# us_murder_rate is stored as a data frame
+class(us_murder_rate)
+
+# the pull function can return it as a numeric value
+us_murder_rate %>% pull(rate)
+
+# using pull to save the number directly
+us_murder_rate <- murders %>% 
+  summarize(rate = sum(total) / sum(population) * 10^5) %>%
+  pull(rate)
+us_murder_rate
+
+# us_murder_rate is now stored as a number
+class(us_murder_rate)
+
+## The dot placeholder allows us to access the dataframe being piped; it is a placeholder for the data being passed through the pipe
+library(tidyverse)
+library(dplyr)
+library(dslabs)
+data(murders)
+murders <- mutate(murders, rate = total / population * 10^5)
+
+# average rate adjusted by population size
+us_murder_rate <- murders %>% 
+  summarize(rate = sum(total) / sum(population) * 10^5)
+us_murder_rate
+
+# using the dot to access the rate
+us_murder_rate <- murders %>% 
+  summarize(rate = sum(total) / sum(population) * 10^5) %>%
+  .$rate
+us_murder_rate
+class(us_murder_rate)
+
+
+## Group then summarize by creating a "grouped data frame"
+library(tidyverse)
+library(dplyr)
+library(dslabs)
+data(murders)
+murders <- mutate(murders, rate = total / population * 10^5)
+
+# group by region
+murders %>% group_by(region)
+
+# summarize after grouping
+murders %>% 
+  group_by(region) %>%
+  summarize(median = median(rate))
+
+## Sorting data tables: when examining a data set, it is often convenient to sort the table using the values in different columns. 
+# For ordering entire tables, the dplyr function "arrange()" is useful.
+ library(tidyverse)
+library(dplyr)
+library(dslabs)
+data(murders)
+murders <- mutate(murders, rate = total / population * 10^5)
+
+# order the states by population size
+murders %>% arrange(population) %>% head()
+
+# order the states by murder rate - the default is ascending order
+murders %>% arrange(rate) %>% head()
+
+# order the states by murder rate in descending order
+murders %>% arrange(desc(rate)) %>% head()
+
+# order the states by region and then by murder rate within region
+murders %>% arrange(region, rate) %>% head()
+
+# return the top 10 states by murder rate
+murders %>% top_n(10, rate)
+
+# return the top 10 states ranked by murder rate, sorted by murder rate
+murders %>% arrange(desc(rate)) %>% top_n(10)
